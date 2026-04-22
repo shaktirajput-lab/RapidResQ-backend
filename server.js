dc644885f3957a8ff51410127f5f854ca69ff5a1
shaktirajput-lab <chhagand9@gmail.com>
2026-04-22 20:30:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const admin = require("firebase-admin");
 
 dotenv.config();
 
-// Firebase config
-const serviceAccount = require("./serviceAccountKey.json");
+// Firebase configconst admin = require("firebase-admin");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 if (!admin.apps.length) {
   admin.initializeApp({
