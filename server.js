0d581c26947a1dfb72f76b0f5bbe4ed9bfb89a6a
shaktirajput-lab <chhagand9@gmail.com>
2026-04-22 22:22:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ dotenv.config();
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL
+};
 };
 
 admin.initializeApp({
